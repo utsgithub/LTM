6c68b6dce4cc3456fab8349457d6d439d13c6c15
Mahie Rahman <Ashfaqur.M.Rahman@student.uts.edu.au>
2016-04-13 19:53:05 +1000

    Including untracked files to the project

---
+++ IMS.Domain/InterventionTypes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Domain
+{
+    class InterventionTypes
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public int AmountOfHours { get; set; }
+
+        public double CostOfMaterials { get; set; }
+    }
+}
---
+++ IMS.Domain/Users.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Domain
+{
+    class Users
+    {
+        public int ID { get; set; }
+
+        public string Username { get; set; }
+
+        public string LoginName { get; set; }
+
+        public string Password { get; set; }
+
+        public Districts district { get; set; }
+
+        public int MaxHours { get; set; }
+
+        public double MaxCost { get; set; }
+
+        public UserType type { get; set; }
+
+    }
+}
